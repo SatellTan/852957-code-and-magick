8bc5252dc72aa8ac9f4e248befce313acb031078
Tatyana <taniaks2003@mail.ru>
2018-12-14 03:08:57 +0500

    Доработан блок сообщений об ошибках

--- js/setup.js
+++ js/setup.js
@@ -3,12 +3,9 @@
 (function () {
   var WIZARDS_NUMBER = 4;
 
-  // var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-  // var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
   var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
   var fireballColors = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
-  // var wizards = [];
 
   var setup = document.querySelector('.setup');
   var similarListElement = document.querySelector('.setup-similar-list');
@@ -25,10 +22,6 @@
     return array[Math.floor(Math.random() * array.length)];
   };
 
-  /* var createName = function () {
-    return itemSelection(names) + ' ' + itemSelection(surnames);
-  };*/
-
   var renderWizard = function (wizard) {
     var wizardElement = similarWizardTemplate.cloneNode(true);
 
@@ -38,27 +31,6 @@
 
     return wizardElement;
   };
-
-  /* var fillingBlock = function (array) {
-    var fragment = document.createDocumentFragment();
-
-    for (var i = 0; i < array.length; i++) {
-      fragment.appendChild(renderWizard(array[i]));
-    }
-
-    return fragment;
-  };
-
-  for (var i = 0; i < WIZARDS_NUMBER; i++) {
-    wizards[i] = {
-      name: createName(),
-      coatColor: itemSelection(coatColors),
-      eyesColor: itemSelection(eyesColors)
-    };
-  }
-
-  similarListElement.appendChild(fillingBlock(wizards));
-  setup.querySelector('.setup-similar').classList.remove('hidden');*/
 
   wizardCoat.addEventListener('click', function () {
 
